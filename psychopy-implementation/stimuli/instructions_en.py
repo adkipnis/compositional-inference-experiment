ee6d775c96725dfb4269548ee82c0f3fc683a56a
Alex <adkipnis@mailbox.org>
2021-08-06 15:59:57 +0200

    Instructions for Visual Cue Training and the two Test Trainings

--- psychopy-implementation/stimuli/instructions_en.py
+++ psychopy-implementation/stimuli/instructions_en.py
@@ -35,7 +35,7 @@ def AddProceedKey2All(instruction_list, proceed_key, wait_s = 3):
 
 
 
-# Navigation
+# Navigation Introduction
 Navigation = [["For the following displays, you can navigate back and forth"\
                    " using the arrow keys:", "/e", 4],
               0,
@@ -49,9 +49,11 @@ Navigation = [["For the following displays, you can navigate back and forth"\
               1,
               "Please be careful not to press this key by accident!",
               "Finally, if you have any questions, please ask the examiner.",
-              "Now, the experiment begins...",
-              ["", "/t", 2]]
-# Introduction
+              "Now, the experiment begins..."
+              # ["", "/t", 2]
+              ]
+
+# Story Introduction 
 Intro = ["You are a mage apprentice, preparing for your Alteration classes.",
          "Luckily, you own a copy of the famous book series"\
              " 'Principles of Alteration Magic', written by Philbertine.",
@@ -73,7 +75,7 @@ Intro = ["You are a mage apprentice, preparing for your Alteration classes.",
     ]
     
 
-# Before textual practice
+# Text Cues
 Intermezzo1 = ["Now that you have committed these spells to your memory, you"\
                    " want to make sure you remember them correctly.",
                "In her wise anticipation, Philbertine provides you with a"\
@@ -96,23 +98,58 @@ Feedback1 = ["In this training run, your score was:",
              0, 
              "This is good, you are confident to make the next step..."]
 
-# Before visual practice
+# Visual Cues
 NowVisual = ["You have now learned the names of the basic spells.",
-             "In reality, you sometimes need to cast a spell without saying"\
-                 " it out loud.",
-             "In fact, each spell also has a specific symbol.",
-             "Yes, you can also cast spells by focusing on that symbol"\
-                 " in your mind.",
-             "You decide to spend a couple more minutes to study these...",
+             "However, Filbertine writes of the 'Duality' of spells:",
+             "Each spell has a specific name and symbol.",
+             "You need to be able to cast a spell after seeing its symbol,",
+             "So you decide to spend a couple more minutes to study these...",
              "When you are done with studying, you may press the spacebar.",
              "Ready?"]
     
-Intermezzo2 = ["Now that you have also learned to cast spells using symbols, you"\
+Intermezzo2 = ["Now that you have also learned all relevant spell symbols, you"\
                    " want to make sure you remember them correctly.",
                "Again, Philbertine provides you with a magical practice board.",
                "The instructions are completely analogous to the previous"\
                    " practice board.",
                "Ready?"]
+    
+# Positional Test
+NowPosition1 = [0,
+               "It is time for some field practice.",
+               "You now know the spell names and symbols that prompt you to "\
+                   "cast a spell.",
+               "You will now practice applying these spells to groups of objects.",
+               "Earlier, you saw what effect transformation spells had.",
+               "For the following trials, you will first see the spell cue "\
+                   "and then the objects you need to transform using it."]
+
+NowPosition2 = ["Here is what you do:",
+                "(1) Memorize the presented objects.",
+                "(2) After they disappear, transform the objects in your mind.",
+                "When you are done: (3) Press the spacebar.",
+                "You will see all the squares on which the objects stood, "\
+                    "one of them will be marked.",
+                "(4) From the options below, choose the object that would be "\
+                    "on the marked square after the transformation.",
+                "Use the following keys for this choice:",
+                0,
+                "We will do 3 slow trials first."]
+NowPosition3 = ["Next, you will do normal paced trials.",
+                "Ready?"]
+
+# Counting Test
+NowCount1 = ["Some magic students tend to cheat on the test above.",
+            "In order to make sure that you transform the objects in your mind "\
+                "first and only then answer a test question...",
+            "...there are two test displays (both equally likely to appear):",
+            "The previous one was based on object positions. The next will be based "\
+                "on the object count.",
+            "After transforming the objects (1-3), you will now see an object category",
+            "(4) From the options below, choose how often "\
+                    "this object appears in the scene after the transformation.",
+            "We will do 3 slow trials first."] 
+
     
 # Store as Dictionary
 instructions ={  
@@ -124,7 +161,11 @@ instructions ={
   "Intermezzo2": AddProceedKey2All(Intermezzo2, '/k'),
   "Feedback0": AddProceedKey2All(Feedback0, '/e', wait_s = 2),
   "Feedback1": AddProceedKey2All(Feedback1, '/e', wait_s = 2),
-  "NowVisual": AddProceedKey2All(NowVisual, '/k')
+  "NowVisual": AddProceedKey2All(NowVisual, '/k'),
+  "NowPosition1": AddProceedKey2All(NowPosition1, '/k'),
+  "NowPosition2": AddProceedKey2All(NowPosition2, '/k'),
+  "NowPosition3": AddProceedKey2All(NowPosition3, '/k'),
+  "NowCount1": AddProceedKey2All(NowCount1, '/k')
 }  
 
 with open('instructions_en.pkl', 'wb') as handle:
