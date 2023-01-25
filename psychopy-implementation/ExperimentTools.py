28b861ae26c0e1ab7c91f39f30122728ce6236ff
adkipnis <adkipnis@mailbox.org>
2023-01-25 11:37:39 +0100

    Session 1 is ready for full testing

--- psychopy-implementation/ExperimentTools.py
+++ psychopy-implementation/ExperimentTools.py
@@ -1480,65 +1480,65 @@ class Experiment:
 
         # Navigation
         self.win.mouseVisible = False
-        # self.Instructions(part_key="Navigation1",
-        #                   special_displays=[self.iSingleImage,
-        #                                     self.iSingleImage],
-        #                   args=[self.keyboard_dict["keyBoardArrows"],
-        #                         self.keyboard_dict["keyBoardEsc"]],
-        #                   font="mono",
-        #                   fontcolor=self.color_dict["mid_grey"],
-        #                   show_background=False)
+        self.Instructions(part_key="Navigation1",
+                          special_displays=[self.iSingleImage,
+                                            self.iSingleImage],
+                          args=[self.keyboard_dict["keyBoardArrows"],
+                                self.keyboard_dict["keyBoardEsc"]],
+                          font="mono",
+                          fontcolor=self.color_dict["mid_grey"],
+                          show_background=False)
 
-        # # Introduction
-        # self.Instructions(part_key="Intro",
-        #                   special_displays=[self.iSingleImage,
-        #                                     self.iSingleImage,
-        #                                     self.iTransmutableObjects,
-        #                                     self.iSpellExample,
-        #                                     self.iSpellExample],
-        #                   args=[self.magicBooks,
-        #                         self.philbertine,
-        #                         None,
-        #                         [["A", "B", "C", "D"], ["A", "D", "C", "D"]],
-        #                         [["A", "B", "B", "D"], ["A", "D", "D", "D"]]])
+        # Introduction
+        self.Instructions(part_key="Intro",
+                          special_displays=[self.iSingleImage,
+                                            self.iSingleImage,
+                                            self.iTransmutableObjects,
+                                            self.iSpellExample,
+                                            self.iSpellExample],
+                          args=[self.magicBooks,
+                                self.philbertine,
+                                None,
+                                [["A", "B", "C", "D"], ["A", "D", "C", "D"]],
+                                [["A", "B", "B", "D"], ["A", "D", "D", "D"]]])
 
 
-        # ''' --- 2. Learn Cues --------------------------------------------------------'''
-        # # Balance out which cue modality is learned first
-        # first_modality = "visual" if id_is_odd else "textual"
-        # second_modality = "textual" if id_is_odd else "visual"
+        ''' --- 2. Learn Cues --------------------------------------------------------'''
+        # Balance out which cue modality is learned first
+        first_modality = "visual" if id_is_odd else "textual"
+        second_modality = "textual" if id_is_odd else "visual"
 
-        # # Learn first cue type
-        # self.learnDuration_1 = self.LearnCues()
-        # with open(f"{self.file_name}.txt", 'a') as f:
-        #     f.write(f"learnDuration_1 = {self.learnDuration_1}\n")
+        # Learn first cue type
+        self.learnDuration_1 = self.LearnCues()
+        with open(f"{self.file_name}.txt", 'a') as f:
+            f.write(f"learnDuration_1 = {self.learnDuration_1}\n")
 
-        # # Test first cue type
-        # self.Instructions(part_key="Intermezzo1",
-        #                   special_displays=[self.iSingleImage],
-        #                   args=[self.keyboard_dict[f"keyBoard{self.n_cats}"]]
-        #                   )
-        # self.df_out_1 = self.CuePracticeLoop(self.trials_prim_cue,
-        #                                      i_step = 2 if self.test_mode else None,
-        #                                      mode=first_modality)
+        # Test first cue type
+        self.Instructions(part_key="Intermezzo1",
+                          special_displays=[self.iSingleImage],
+                          args=[self.keyboard_dict[f"keyBoard{self.n_cats}"]]
+                          )
+        self.df_out_1 = self.CuePracticeLoop(self.trials_prim_cue,
+                                             i_step = 2 if self.test_mode else None,
+                                             mode=first_modality)
         
-        # # Learn second cue type
-        # self.Instructions(part_key="Intermezzo2",
-        #                   special_displays=[self.iSingleImage],
-        #                   args=[self.keyboard_dict[f"keyBoard{self.n_cats}"]])
-        # self.learnDuration_2 = self.LearnCues()
-        # with open(f"{self.file_name}.txt", 'a') as f:
-        #     f.write(f"learnDuration_2 = {self.learnDuration_2}\n")
+        # Learn second cue type
+        self.Instructions(part_key="Intermezzo2",
+                          special_displays=[self.iSingleImage],
+                          args=[self.keyboard_dict[f"keyBoard{self.n_cats}"]])
+        self.learnDuration_2 = self.LearnCues()
+        with open(f"{self.file_name}.txt", 'a') as f:
+            f.write(f"learnDuration_2 = {self.learnDuration_2}\n")
 
-        # # Test second cue type
-        # self.df_out_2 = self.CuePracticeLoop(self.trials_prim_cue,
-        #                                      i_step = 2 if self.test_mode else None,
-        #                                      mode=second_modality,
-        #                                      i=len(self.df_out_1))
+        # Test second cue type
+        self.df_out_2 = self.CuePracticeLoop(self.trials_prim_cue,
+                                             i_step = 2 if self.test_mode else None,
+                                             mode=second_modality,
+                                             i=len(self.df_out_1))
 
-        # # Save cue memory data
-        # fname = f"{self.data_dir}{os.sep}{self.expInfo['participant']}_{self.expInfo['dateStr']}_cueMemory"
-        # save_object(self.df_out_1 + self.df_out_2, fname, ending='csv')
+        # Save cue memory data
+        fname = f"{self.data_dir}{os.sep}{self.expInfo['participant']}_{self.expInfo['dateStr']}_cueMemory"
+        save_object(self.df_out_1 + self.df_out_2, fname, ending='csv')
 
 
         ''' --- 3. Test Types --------------------------------------------------------'''
