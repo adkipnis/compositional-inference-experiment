58f2db08896847023b637bb7753f0c7f49f52678
adkipnis <adkipnis@mailbox.org>
2023-01-25 15:31:47 +0100

    further rescaling instructions

--- psychopy-implementation/stimuli/resize_images.sh
+++ psychopy-implementation/stimuli/resize_images.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-for file in c*.png;
+# cues
+for file in c_*.png;
   do convert $file -scale 256x256 $file;
 done
+
+# stimuli
+convert s_globe.png -scale 180x180 s_globe.png
+convert s_shoe.png -scale 190x190 s_shoe.png
+convert s_frog.png -scale 200x200 s_frog.png
+convert s_puzzle.png -scale 200x200 s_puzzle.png
+convert s_banana.png -scale 210x210 s_banana.png
+convert s_signpost.png -scale 220x220 s_signpost.png
+convert s_rocket.png -scale 220x220 s_rocket.png
+convert s_heart.png -scale 230x230 s_heart.png
+
+# rest
+convert magicBooks.png -scale 60% magicBooks.png
 
