96a6afd29a7c341183cd1c69b0b71ea210621e24
adkipnis <adkipnis@mailbox.org>
2023-02-02 12:13:50 +0100

    residual changes

--- psychopy-implementation/main.py
+++ psychopy-implementation/main.py
@@ -7,14 +7,14 @@ from ExperimentTools import Experiment
 
 # Initialize
 Exp = Experiment()
-Exp.dialogue_box(show=True, participant=1)
-Exp.init_window(screen=0, fullscr=True)
+Exp.dialogue_box(show=True, participant=1, session=2)
+Exp.init_window(screen=0, fullscr=False)
 Exp.load_trials()
 Exp.render_visuals()
 Exp.init_progbar()
 
 # Start Experiment
-if Exp.expInfo["session"] in ['1', '2']:
+if Exp.expInfo['session'] in [1, 2]:
     eval(f"Exp.Session{Exp.expInfo['session']}()")
 else:
     raise ValueError('A session with this number does not exist')
