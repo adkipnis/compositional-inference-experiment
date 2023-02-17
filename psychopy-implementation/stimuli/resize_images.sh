e8f2359a04fe946ab3f5debe316e949deb0b43f7
adkipnis <adkipnis@mailbox.org>
2023-02-17 14:31:22 +0100

    resized images again

--- psychopy-implementation/stimuli/resize_images.sh
+++ psychopy-implementation/stimuli/resize_images.sh
@@ -11,14 +11,14 @@ for file in keyBoard*.png;
 done
 
 # stimuli
-convert s_globe.png -scale 170x170 s_globe.png
-convert s_shoe.png -scale 190x190 s_shoe.png
-convert s_frog.png -scale 190x190 s_frog.png
-convert s_puzzle.png -scale 200x200 s_puzzle.png
-convert s_banana.png -scale 210x210 s_banana.png
-convert s_signpost.png -scale 220x220 s_signpost.png
-convert s_rocket.png -scale 220x220 s_rocket.png
-convert s_heart.png -scale 230x230 s_heart.png
+convert s_globe.png -scale 160x160 s_globe.png
+convert s_shoe.png -resize 160x190! s_shoe.png
+convert s_frog.png -scale 180x180 s_frog.png
+convert s_puzzle.png -scale 190x190 s_puzzle.png
+convert s_banana.png -scale 200x200 s_banana.png
+convert s_signpost.png -scale 210x210 s_signpost.png
+convert s_rocket.png -scale 210x210 s_rocket.png
+convert s_heart.png -scale 220x220 s_heart.png
 
 # rest
 convert leftArrow.png -scale 128x128 leftArrow.png
