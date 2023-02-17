aa6eb5c97ec12e3df685d703e2c7db3dab0b9ced
adkipnis <adkipnis@mailbox.org>
2023-02-17 18:19:00 +0100

    update main

--- psychopy-implementation/main.py
+++ psychopy-implementation/main.py
@@ -7,8 +7,8 @@ from ExperimentTools import Experiment
 
 # Initialize
 exp = Experiment()
-exp.dialogue_box(show=False, participant=1, session=1,test_mode=False)
-exp.init_window(screen=0, fullscr=False)
+exp.dialogue_box(show=True, participant=1, session=1,test_mode=False)
+exp.init_window(screen=0, fullscr=True)
 exp.load_trials()
 exp.render_visuals()
 
