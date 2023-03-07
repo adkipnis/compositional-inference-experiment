8ed0d309b4875159ddc4c889638493d7186e06c6
adkipnis <adkipnis@mailbox.org>
2023-03-07 16:16:10 +0100

    test-practice component test updated

---
+++ psychopy-implementation/component-tests/test-practice.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 10 14:31:39 2022
+
+@author: alex
+"""
+
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+from ExperimentTools import Experiment
+
+# Initialize
+exp = Experiment()
+exp.dialogue_box(show=False, participant=1, session=1, test_mode=False)
+exp.init_window(screen=0, fullscr=True)
+exp.load_trials()
+exp.render_visuals()
+exp.init_progbar()
+
+# Run Cue Practice Session task
+out = exp.adaptiveBlock(exp.trials_prim_prac_c, streak_goal=1)
+exp.win.close()
