e62b36f0545f74a90d2becb5ac3c94ad70a0b1de
adkipnis <adkipnis@mailbox.org>
2023-03-08 20:36:29 +0100

    abstracted pathname

--- psychopy-implementation/component-tests/set-mapping-lists-for-piloting.py
+++ psychopy-implementation/component-tests/set-mapping-lists-for-piloting.py
@@ -1,5 +1,9 @@
 import pickle
-fname = "/kyb/rg/akipnis/Nextcloud/Code/compositional-inference-experiment/psychopy-implementation/trial-lists/01_mappinglists.pkl"
+import os
+abspath = os.path.abspath(__file__)
+relpath = os.path.relpath(os.path.realpath(__file__))
+trunk = abspath.replace(relpath, "")
+fname = os.path.join(trunk, "trial-lists/01_mappinglists.pkl")
 m = pickle.load(open(fname, "rb"))
 m["stim"] = ['s_heart', 's_shoe', 's_frog', 's_puzzle',
              's_globe', 's_banana', 's_signpost', 's_rocket']
