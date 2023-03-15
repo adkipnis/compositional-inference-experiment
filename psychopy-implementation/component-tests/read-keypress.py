25dcdc0164879df339d04623b64be6746e780062
adkipnis <adkipnis@mailbox.org>
2023-03-15 18:15:50 +0100

    updated read-keypress

--- psychopy-implementation/component-tests/read-keypress.py
+++ psychopy-implementation/component-tests/read-keypress.py
@@ -7,20 +7,19 @@ Created on Fri May 27 15:58:02 2022
 
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-from ExperimentTools import Experiment
 from psychopy import event, core
+from ExperimentTools import Experiment
 
-# Initialize
-Exp = Experiment()
-Exp.init_window(screen = 1)
-# Exp.init_interface()
+exp = Experiment()
+exp.dialogue_box(show=False, participant=1, session=1, test_mode=True)
+exp.init_window(screen=1, fullscr=False)
 
 # Test
 allKeys = event.waitKeys(maxWait = 10)
 print(allKeys[0])
-testRT, testResp = Exp.tTestresponse(
+testRT, testResp = exp.tTestResponse(
     core.Clock(),
-    Exp.resp_keys #Exp.resp_keys_vpixx
+    exp.resp_keys #exp.resp_keys_vpixx
     )
 print(testResp)
-Exp.win.close()
+exp.win.close()
