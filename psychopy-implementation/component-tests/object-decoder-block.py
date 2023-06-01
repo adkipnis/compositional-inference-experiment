5a8aca30567ebebb957a6476529fa978dcb35702
adkipnis <adkipnis@mailbox.org>
2023-06-01 19:35:16 +0200

    component test for object decoder trials

--- psychopy-implementation/component-tests/object-decoder-block.py
+++ psychopy-implementation/component-tests/object-decoder-block.py
@@ -7,22 +7,32 @@ Created on Wed Jul  6 11:18:02 2022
 """
 
 import os, sys
+import numpy as np
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from ExperimentTools import Experiment
 
 # Initialize
-Exp = Experiment()
-Exp.init_window(screen = 1, fullscr = True)
-Exp.dialoguebox(show = False, session = "1")
-Exp.expInfo["session"] = "3"
-Exp.load_trials()
-Exp.render_visuals()
-Exp.init_progbar(bar_pos = [0, 15])
-Exp.win.mouseVisible = False
+exp = Experiment()
+exp.dialogue_box(show=False, participant=1, session=1, test_mode=False)
+exp.init_window(screen=1, fullscr=True)
+exp.load_trials()
+exp.render_visuals()
+exp.init_progbar()
 
-# Run Cue Practice Session task
-Exp.start_width = 0
-df_out, accuracy = Exp.LocalizerBlock(
-    Exp.trials_localizer,
-    durations = [2.0, 2.0, 2.0, 1.0])
-Exp.win.close()
+# Run Object Decoder block 
+trial0 = {'trial_type': 'object_decoder',
+         'input_disp': np.array([None, 'B', None, None]),
+         'is_catch_trial': False,
+         'correct_resp': None,
+         'jitter': -0.027}
+
+trial1 = {'trial_type': 'object_decoder',
+         'input_disp': np.array(['A', None, None, None]),
+         'is_catch_trial': True,
+         'target': 'A',
+         'correct_resp': True,
+         'jitter': 0.027}
+exp.objectDecoderTrial(trial0, fixation_duration=0.3 + trial0["jitter"],)
+exp.objectDecoderTrial(trial1, fixation_duration=0.3 + trial1["jitter"],)
+
+exp.win.close()
