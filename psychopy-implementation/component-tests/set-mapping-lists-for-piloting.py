ce9ffc5f1ac63b9450df2fad7e5553e63ab0ad95
adkipnis <adkipnis@mailbox.org>
2023-06-01 17:49:22 +0200

    corrected os.path.join

--- psychopy-implementation/component-tests/set-mapping-lists-for-piloting.py
+++ psychopy-implementation/component-tests/set-mapping-lists-for-piloting.py
@@ -3,7 +3,7 @@ import os
 abspath = os.path.abspath(__file__)
 relpath = os.path.relpath(os.path.realpath(__file__))
 trunk = abspath.replace(relpath, "")
-fname = os.path.join(trunk, "psychopy-implementation/trial-lists/01_mappinglists.pkl")
+fname = os.path.join(trunk, "psychopy-implementation", "trial-lists", "01_mappinglists.pkl")
 m = pickle.load(open(fname, "rb"))
 m["stim"] = ['s_heart', 's_shoe', 's_frog', 's_puzzle',
              's_globe', 's_banana', 's_signpost', 's_rocket']
