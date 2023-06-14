cefa94aa1d830e55436fe305e2c5b9dd59d29bb9
adkipnis <adkipnis@mailbox.org>
2023-06-14 10:36:07 +0200

    print statements added to set-mapping-lists...

--- psychopy-implementation/component-tests/set-mapping-lists-for-piloting.py
+++ psychopy-implementation/component-tests/set-mapping-lists-for-piloting.py
@@ -3,6 +3,7 @@ import os
 leaf = os.path.join("component-tests", "set-mapping-lists-for-piloting.py")
 trunk = os.path.abspath(__file__).replace(leaf, "")
 fname = os.path.join(trunk, "trial-lists", "01_mappinglists.pkl")
+print("Path to mapping lists:", fname)
 m = pickle.load(open(fname, "rb"))
 m["stim"] = ['s_heart', 's_shoe', 's_frog', 's_puzzle',
              's_globe', 's_banana', 's_signpost', 's_rocket']
@@ -13,3 +14,4 @@ m["vcue"] = ['c_F', 'c_A', 'c_E',
 
 with open(fname, "wb") as f:
     pickle.dump(m, f)
+    print("Changed mapping lists suffecfully.")
