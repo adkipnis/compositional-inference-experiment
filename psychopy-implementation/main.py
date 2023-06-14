529d617d72d5352c3e0ddf83dd8e0a4cb5ab5f4a
adkipnis <adkipnis@mailbox.org>
2023-06-14 10:35:44 +0200

    main prepared for testing

--- psychopy-implementation/main.py
+++ psychopy-implementation/main.py
@@ -7,7 +7,13 @@ from ExperimentTools import Experiment
 
 # Initialize
 exp = Experiment()
-exp.dialogue_box(show=False, participant=1, session=1, meg=False, test_mode=True)
+exp.dialogue_box(
+    show=False,
+    participant=1,
+    session=1,
+    meg=True,
+    test_mode=False,
+    )
 exp.init_window(screen=0, fullscr=True)
 exp.load_trials()
 exp.render_visuals()
