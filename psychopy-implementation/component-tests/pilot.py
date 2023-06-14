35186672de04c42e05cfc3384fcf540124669d4f
adkipnis <adkipnis@mailbox.org>
2023-06-14 11:48:18 +0200

    option dictionaries

--- psychopy-implementation/component-tests/pilot.py
+++ psychopy-implementation/component-tests/pilot.py
@@ -2,16 +2,18 @@ import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from ExperimentTools import Experiment
 
+expOpts = {"show": False,
+           "participant": 1,
+           "session": 1,
+           "meg": False,
+           "test_mode": True}
+winOpts = {"screen": 0,
+           "fullscr": True}
+
 # Initialize
 exp = Experiment()
-exp.dialogue_box(
-    show=False,
-    participant=1,
-    session=1,
-    meg=False,
-    test_mode=False,
-    )
-exp.init_window(screen=0, fullscr=True)
+exp.dialogue_box(**expOpts)
+exp.init_window(**winOpts)
 exp.load_trials()
 exp.render_visuals()
 
@@ -20,7 +22,8 @@ exp.Session1()
 exp.win.close()
 
 # Session 2
-exp.init_window(screen=0, fullscr=True)
-exp.session = 2
+expOpts.update({"session": 2})
+exp.dialogue_box(**expOpts)
+exp.init_window(**winOpts)
 exp.Session2()
 exp.win.close()
