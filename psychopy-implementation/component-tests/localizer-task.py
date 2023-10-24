479e4f1cdf5bf62530c69f796436c8adce24000e
adkipnis <adkipnis@mailbox.org>
2023-10-24 12:23:46 +0200

    update component tests

--- psychopy-implementation/component-tests/localizer-task.py
+++ psychopy-implementation/component-tests/localizer-task.py
@@ -5,20 +5,20 @@ Created on Fri May 27 10:55:28 2022
 
 @author: alex
 """
-
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from ExperimentTools import Experiment
 
 # Initialize
-Exp = Experiment()
-Exp.init_window(screen = 1, fullscr = True)
-Exp.dialoguebox(show = False)
-Exp.load_trials()
-Exp.render_visuals()
-Exp.init_progbar(bar_pos = [0, 15])
-Exp.win.mouseVisible = False
+exp = Experiment()
+exp.dialogue_box(show=False, participant=1, session=1, meg=False, test_mode=False)
+exp.init_window(screen=1, fullscr=True)
+exp.load_trials()
+exp.render_visuals()
+exp.init_progbar()
 
 # Run localizer task
-Exp.LocalizerBlock(Exp.trials_localizer, resp_keys = Exp.resp_keys_alt)
-Exp.win.close()
+df_out = exp.adaptiveDecoderBlock(exp.trials_obj_dec[:2], decoderType="object")
+fname = exp.writeFileName("objectDecoder")
+exp.save_object(df_out, fname, ending='csv')
+exp.win.close()
--- psychopy-implementation/component-tests/test-practice.py
+++ psychopy-implementation/component-tests/test-practice.py
@@ -5,15 +5,14 @@ Created on Fri Jun 10 14:31:39 2022
 
 @author: alex
 """
-
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from ExperimentTools import Experiment
 
 # Initialize
 exp = Experiment()
-exp.dialogue_box(show=False, participant=1, session=1, meg=True, test_mode=False)
-exp.init_window(screen=0, fullscr=True)
+exp.dialogue_box(show=False, participant=1, session=1, meg=False, test_mode=False)
+exp.init_window(screen=1, fullscr=True)
 exp.load_trials()
 exp.render_visuals()
 exp.init_progbar()
