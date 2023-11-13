be97f761f3c4c3f6b1692b5b1532e0f2064d62ca
adkipnis <adkipnis@mailbox.org>
2023-11-13 11:39:47 +0100

    listofdicts2csv try transforming to csv except pickle

--- psychopy-implementation/ExperimentTools.py
+++ psychopy-implementation/ExperimentTools.py
@@ -450,51 +450,57 @@ class Experiment:
     def listofdicts2csv(self, trials, fname):
         """ write a list of trialdicts to a csv """
         trials = copy.deepcopy(trials)
-
-        # === data integrity checks ===
-        # 1. check if all trials have the same keys, dump otherwise
-        unique_keys = trials[0].keys()
-        key_check_list = [set(trial.keys()) == set(unique_keys)
-                        for trial in trials]
-        if not all(key_check_list):
-            self.dump(trials, fname, "keys")
-            return
-            
-        # 2. check if all trials have the same item types per key, dump otherwise
-        key_types = {key: type(trials[0][key])
-                     for key in unique_keys}
-        type_check_list = [type(trial[key]) == key_types[key]
-                           for trial in trials for key in unique_keys if key != "target"]
-        if not all(type_check_list):
-            self.dump(trials, fname, "key types")
-            return
         
-        # === data transformations ===
-        # spread out lists and sets to new enumerated keys
-        original_keys = list(unique_keys)
-        list_keys = [key for key in original_keys
-                    if isinstance(trials[0][key], (set, list, np.ndarray))]
-        if list_keys:
-            new_keys = []
-            for trial in trials:
-                for key in list_keys:
-                    trial_updates = {f"{key}_{i}": item
-                                    for i, item in enumerate(trial[key])}
-                    trial.update(trial_updates)
-                    new_keys.extend(trial_updates.keys())
-                    del trial[key]
+        try:
+            # === data integrity checks ===
+            # 1. check if all trials have the same keys, dump otherwise
+            unique_keys = trials[0].keys()
+            key_check_list = [set(trial.keys()) == set(unique_keys)
+                            for trial in trials]
+            if not all(key_check_list):
+                self.dump(trials, fname, "keys")
+                return
+                
+            # 2. check if all trials have the same item types per key, dump otherwise
+            key_types = {key: type(trials[0][key])
+                        for key in unique_keys}
+            type_check_list = [type(trial[key]) == key_types[key]
+                            for trial in trials for key in unique_keys if key != "target"]
+            if not all(type_check_list):
+                self.dump(trials, fname, "key types")
+                return
             
-            # update unique_keys (some trials may have more than others) and sort them in original order
-            new_keys = sorted(set(new_keys))
-            unique_keys = np.setdiff1d(original_keys, list_keys)
-            unique_keys = sorted(np.union1d(unique_keys, new_keys))
-    
-        # === write to csv ===
-        with open(fname + ".csv", "w", newline="") as output_file:
-            dict_writer = csv.DictWriter(output_file, unique_keys)
-            dict_writer.writeheader()
-            dict_writer.writerows(trials)
-            print(f"Saved {len(trials)} trials to '{fname}.csv'")
+            # === data transformations ===
+            # spread out lists and sets to new enumerated keys
+            original_keys = list(unique_keys)
+            list_keys = [key for key in original_keys
+                        if isinstance(trials[0][key], (set, list, np.ndarray))]
+            if list_keys:
+                new_keys = []
+                for trial in trials:
+                    for key in list_keys:
+                        trial_updates = {f"{key}_{i}": item
+                                        for i, item in enumerate(trial[key])}
+                        trial.update(trial_updates)
+                        new_keys.extend(trial_updates.keys())
+                        del trial[key]
+                
+                # update unique_keys (some trials may have more than others) and sort them in original order
+                new_keys = sorted(set(new_keys))
+                unique_keys = np.setdiff1d(original_keys, list_keys)
+                unique_keys = sorted(np.union1d(unique_keys, new_keys))
+        
+            # === write to csv ===
+            with open(fname + ".csv", "w", newline="") as output_file:
+                dict_writer = csv.DictWriter(output_file, unique_keys)
+                dict_writer.writeheader()
+                dict_writer.writerows(trials)
+                print(f"Saved {len(trials)} trials to '{fname}.csv'")
+                
+        except Exception as e:
+            print(e)
+            self.dump(trials, fname, "other exception")
+            return
             
 
     def save_object(self, obj, fname, ending='pkl'):
